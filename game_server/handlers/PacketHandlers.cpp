ee33702cccd4a3465283e4753550cb4ef846951a
Chnossos <chnossos@gmail.com>
2026-03-03 17:54:01 +0100

    add: client opcodes enum

--- game_server/handlers/PacketHandlers.cpp
+++ game_server/handlers/PacketHandlers.cpp
@@ -3,14 +3,15 @@
 
 #include "PacketHandlers.hpp"
 
+// Project includes
+#include "../network/packets/client/OpCodes.hpp"
+
 namespace Handler
 {
 #define HANDLER(name) void handle ## name(Player &);
-    HANDLER(Protocol)
     HANDLER(Authenticate)
     HANDLER(CharacterCancelDeletion)
     HANDLER(CharacterCreate)
-    HANDLER(CharacterList)
     HANDLER(CharacterSelect)
     HANDLER(CharacterShowCreationScreen)
     HANDLER(ConnectionClosing)
@@ -20,6 +21,7 @@ namespace Handler
     HANDLER(MiniMapShow)
     HANDLER(Move)
     HANDLER(MoveUpdate)
+    HANDLER(Protocol)
     HANDLER(QuestList)
     HANDLER(ShortcutBarAdd)
     HANDLER(ShortcutBarRemove)
@@ -32,26 +34,26 @@ namespace Handler
 
 std::unordered_map<unsigned char, std::pair<PacketHandler, std::string_view>> gPacketHandlers
 {
-#define HANDLER(opCode, name) { opCode, {&Handler::handle ## name, #name} }
-    HANDLER(0x00, Protocol),
-    HANDLER(0x08, Authenticate),
-    HANDLER(0x01, Move),
-    HANDLER(0x03, EnterWorld),
-    HANDLER(0x04, TargetSelect),
-    HANDLER(0x09, ConnectionClosing),
-    HANDLER(0x0b, CharacterCreate),
-    HANDLER(0x0d, CharacterSelect),
-    HANDLER(0x0e, CharacterShowCreationScreen),
-    HANDLER(0x2f, SkillUse),
-    HANDLER(0x33, ShortcutBarAdd),
-    HANDLER(0x35, ShortcutBarRemove),
-    HANDLER(0x37, TargetClear),
-    HANDLER(0x3f, SkillList),
-    HANDLER(0x46, LeaveWorld),
-    HANDLER(0x48, MoveUpdate),
-    HANDLER(0x62, CharacterCancelDeletion),
-    HANDLER(0x63, QuestList),
-    HANDLER(0xcd, MiniMapShow),
-    HANDLER(0xd0, ManorList),
+#define HANDLER(name) { std::to_underlying(Network::OpCode::Client::name), {&Handler::handle ## name, #name} }
+    HANDLER(Authenticate),
+    HANDLER(CharacterCancelDeletion),
+    HANDLER(CharacterCreate),
+    HANDLER(CharacterSelect),
+    HANDLER(CharacterShowCreationScreen),
+    HANDLER(ConnectionClosing),
+    HANDLER(EnterWorld),
+    HANDLER(LeaveWorld),
+    HANDLER(ManorList),
+    HANDLER(MiniMapShow),
+    HANDLER(Move),
+    HANDLER(MoveUpdate),
+    HANDLER(Protocol),
+    HANDLER(QuestList),
+    HANDLER(ShortcutBarAdd),
+    HANDLER(ShortcutBarRemove),
+    HANDLER(SkillList),
+    HANDLER(SkillUse),
+    HANDLER(TargetClear),
+    HANDLER(TargetSelect),
 #undef HANDLER
 };
---
+++ game_server/network/packets/client/OpCodes.hpp
@@ -0,0 +1,33 @@
+/// @author    Chnossos
+/// @date      Created on 2026-03-03
+
+#pragma once
+
+// Project includes
+#include <l2cpp/Typedefs.hpp>
+
+namespace Network::OpCode { enum class Client : byte; }
+
+enum class Network::OpCode::Client : byte
+{
+    Protocol                    = 0x00,
+    Authenticate                = 0x08,
+    Move                        = 0x01,
+    EnterWorld                  = 0x03,
+    TargetSelect                = 0x04,
+    ConnectionClosing           = 0x09,
+    CharacterCreate             = 0x0b,
+    CharacterSelect             = 0x0d,
+    CharacterShowCreationScreen = 0x0e,
+    SkillUse                    = 0x2f,
+    ShortcutBarAdd              = 0x33,
+    ShortcutBarRemove           = 0x35,
+    TargetClear                 = 0x37,
+    SkillList                   = 0x3f,
+    LeaveWorld                  = 0x46,
+    MoveUpdate                  = 0x48,
+    CharacterCancelDeletion     = 0x62,
+    QuestList                   = 0x63,
+    MiniMapShow                 = 0xcd,
+    ManorList                   = 0xd0,
+};
