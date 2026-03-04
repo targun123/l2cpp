37b2447d26d03ad12d33e2886257b177bc473b39
Chnossos <chnossos@gmail.com>
2026-03-05 00:11:25 +0100

    improve(CharacterCreate): read race base stats

--- game_server/handlers/CharacterCreate.cpp
+++ game_server/handlers/CharacterCreate.cpp
@@ -11,13 +11,24 @@ DEFINE_PACKET_HANDLER(CharacterCreate)
 {
     PacketReader reader(player.connection().readBuffer().subspan(3));
 
-    u32 ignore;
-
     auto & c = player.addCharacter();
-    reader >> c.name >> c.raceId >> c.sex >> c.classId >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
-           >> c.hairStyleId >> c.hairColorId >> c.faceId;
-    c.selected = 1;
+    reader
+        >> c.name
+        >> c.raceId
+        >> c.sex
+        >> c.classId
+        >> c.baseStats.INT
+        >> c.baseStats.STR
+        >> c.baseStats.CON
+        >> c.baseStats.MEN
+        >> c.baseStats.DEX
+        >> c.baseStats.WIT
+        >> c.hairStyleId
+        >> c.hairColorId
+        >> c.faceId
+    ;
 
+    c.selected = 1;
     player.connection().send(Packet(0x19) << 1);
     handleCharacterList(player);
 }
