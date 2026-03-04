db681b3d88749e805f02017f1a3f3d20b8aceec4
Chnossos <chnossos@gmail.com>
2026-03-05 00:17:41 +0100

    improve(CharacterList): base for displaying gear

--- game_server/handlers/CharacterList.cpp
+++ game_server/handlers/CharacterList.cpp
@@ -31,12 +31,44 @@ DEFINE_PACKET_HANDLER(CharacterList)
           << c.xp
           << c.level
           << c.karma
-          << c.pkCount
-          // << c.pvpCount
+          << std::array<u32, 9>{}
         ;
 
-        constexpr std::array<u32, 40> unknown{};
-        p << unknown;
+        // constexpr size_t PAPERDOLL_UNDER = 0;
+        // constexpr size_t PAPERDOLL_LEAR = 1;
+        // constexpr size_t PAPERDOLL_REAR = 2;
+        // constexpr size_t PAPERDOLL_NECK = 3;
+        // constexpr size_t PAPERDOLL_LFINGER = 4;
+        // constexpr size_t PAPERDOLL_RFINGER = 5;
+        // constexpr size_t PAPERDOLL_HEAD = 6;
+        // constexpr size_t PAPERDOLL_RWEAPON = 7;
+        // constexpr size_t PAPERDOLL_SHIELD = 8;
+        // constexpr size_t PAPERDOLL_GLOVES = 9;
+        // constexpr size_t PAPERDOLL_CHEST = 10;
+        // constexpr size_t PAPERDOLL_LEGS = 11;
+        // constexpr size_t PAPERDOLL_FEET = 12;
+        // constexpr size_t PAPERDOLL_CLOAK = 13;
+        // constexpr size_t PAPERDOLL_LWEAPON = 14;
+        // constexpr size_t PAPERDOLL_HAIR = 15;
+        constexpr size_t PAPERDOLL_TOTALSLOTS = 16;
+
+        /*<item id="6680" type="Armor" name="Sealed Draconic Leather Armor"
+        <item id="6681" type="Armor" name="Sealed Draconic Leather Glove"
+        <item id="6682" type="Armor" name="Sealed Draconic Leather Boots"
+        <item id="6683" type="Armor" name="Sealed Draconic Leather Helmet"*/
+
+        std::array<u32, PAPERDOLL_TOTALSLOTS> gearUids{};
+        std::array<u32, PAPERDOLL_TOTALSLOTS> gearIds{};
+        // gearIds[PAPERDOLL_HEAD] = 6683;
+        // gearIds[PAPERDOLL_RWEAPON] = 6600;
+        // gearIds[PAPERDOLL_LWEAPON] = 6600;
+        // gearIds[PAPERDOLL_SHIELD] = 6678;
+        // gearIds[PAPERDOLL_GLOVES] = 6681;
+        // gearIds[PAPERDOLL_CHEST] = 6680;
+        // gearIds[PAPERDOLL_FEET] = 6682;
+        p << gearUids
+          << gearIds
+        ;
 
         p << c.hairStyleId
           << c.hairColorId
@@ -46,9 +78,7 @@ DEFINE_PACKET_HANDLER(CharacterList)
           << c.deleteTime
           << c.classId
           << c.selected
-          << c.enchantEffect
-          // << c.augmentationId
-          // << c.transformationId
+          << 0_u8 // weapon enchant level (max 127)
         ;
     }
 
