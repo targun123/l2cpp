15fc27c2aba5f1f7f04940129beaae7e601fc5a3
Chnossos <chnossos@gmail.com>
2026-03-06 20:33:04 +0100

    fix(BodyPart): use logical OR instead of logical AND

--- game_server/game/BodyPart.hpp
+++ game_server/game/BodyPart.hpp
@@ -14,11 +14,11 @@ namespace l2
         Underwear     = 0b00000000'00000000'00000001, ///< 0x1
         RightEar      = 0b00000000'00000000'00000010, ///< 0x2
         LeftEar       = 0b00000000'00000000'00000100, ///< 0x4
-        Ears          = RightEar & LeftEar,           ///< 0x6 @warning Not a true slot.
+        Ears          = RightEar | LeftEar,           ///< 0x6 @warning Not a true slot.
         Neck          = 0b00000000'00000000'00001000, ///< 0x8
         RightFinger   = 0b00000000'00000000'00010000, ///< 0x10
         LeftFinger    = 0b00000000'00000000'00100000, ///< 0x20
-        Fingers       = RightFinger & LeftFinger,     ///< 0x30 @warning Not a true slot.
+        Fingers       = RightFinger | LeftFinger,     ///< 0x30 @warning Not a true slot.
         Head          = 0b00000000'00000000'01000000, ///< 0x40
         RightHand     = 0b00000000'00000000'10000000, ///< 0x80
         LeftHand      = 0b00000000'00000001'00000000, ///< 0x100
