bcac4a5252aa80e2e1d4cc3bf9519f86e215f3a1
Chnossos <chnossos@gmail.com>
2026-03-16 16:49:21 +0100

    cleanup: Player

--- game_server/Player.cpp
+++ game_server/Player.cpp
@@ -33,61 +33,30 @@ Player::PlayerImpl::PlayerImpl(boost::asio::ip::tcp::socket && socket)
     characters.emplace_back();
 }
 
+template class Pimpl<Player::PlayerImpl>;
+
+// ---------------------------------------------------------------------------------------------------------------------
+
 Player::Player(boost::asio::ip::tcp::socket && socket)
     : _impl(std::move(socket))
 {}
 
 Player::~Player() = default;
 
-auto Player::connection() -> Network::Connection &
-{
-    return _impl->conn;
-}
+auto Player::connection()       -> Network::Connection       & { return _impl->conn; }
+auto Player::connection() const -> Network::Connection const & { return _impl->conn; }
 
-auto Player::connection() const -> Network::Connection const &
-{
-    return _impl->conn;
-}
+auto Player::characters()       -> std::span<Character>       { return _impl->characters;       }
+auto Player::characters() const -> std::span<Character const> { return _impl->characters;       }
+auto Player::currentCharacter() -> OptionalRef<Character>     { return _impl->currentCharacter; }
 
-auto Player::accountName() const -> std::wstring_view
-{
-    return _impl->accountName;
-}
+auto Player::actions() -> std::deque<std::unique_ptr<Action>> & { return _impl->actionQueue; }
 
-auto Player::playOk1() const -> u32
-{
-    return _impl->playOk1;
-}
+auto Player::accountName() const -> std::wstring_view { return _impl->accountName; }
+auto Player::playOk1()     const -> u32               { return _impl->playOk1;     }
 
-auto Player::characters() -> std::span<Character>
-{
-    return _impl->characters;
-}
-
-auto Player::characters() const -> std::span<Character const>
-{
-    return _impl->characters;
-}
-
-auto Player::currentCharacter() -> OptionalRef<Character>
-{
-    return _impl->currentCharacter;
-}
-
-auto Player::actions() -> std::deque<std::unique_ptr<Action>> &
-{
-    return _impl->actionQueue;
-}
-
-void Player::setAccountName(std::wstring userName)
-{
-    _impl->accountName = std::move(userName);
-}
-
-void Player::setPlayOk1(u32 playOk1)
-{
-    _impl->playOk1 = playOk1;
-}
+void Player::setAccountName(std::wstring userName) { _impl->accountName = std::move(userName); }
+void Player::setPlayOk1(u32 const playOk1)         { _impl->playOk1 = playOk1;                 }
 
 auto Player::addCharacter() -> Character &
 {
