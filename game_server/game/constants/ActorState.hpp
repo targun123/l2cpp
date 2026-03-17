f0618f9019d65186a239e828aa0abeda8a5540a3
Chnossos <chnossos@gmail.com>
2026-03-17 23:23:25 +0100

    add: ActorState
    + doc

---
+++ game_server/game/constants/ActorState.hpp
@@ -0,0 +1,26 @@
+/// @author    Chnossos
+/// @date      Created on 2026-03-17
+
+#pragma once
+
+enum class ActorState
+{
+    Idle,
+    Sitting,
+    AlikeDead,
+    Trading,
+    PreparingStore,
+    PreparingWorksop,
+    RunningStore,
+    RunningWorkshop,
+    CombatIdle,
+    Attacking,
+    Casting,
+    Moving,
+    Following,
+    Chasing,
+    ChasingTired,
+    ChasingStopped,
+    BackToSpawn,
+    Fishing,
+};
