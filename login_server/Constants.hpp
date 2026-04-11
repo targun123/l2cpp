575b837fad06e504feef6a40ce31859f14b33572
Chnossos <chnossos@gmail.com>
2026-04-12 00:39:43 +0200

    improve(login_server): move Connection into its own class

--- login_server/Constants.hpp
+++ login_server/Constants.hpp
@@ -8,6 +8,10 @@
 
 namespace Constants
 {
+    constexpr auto host = "127.0.0.1"sv;
+    constexpr auto port = 2106;
+
+    constexpr u32  protocol        = 0xc621;
     constexpr byte blowfishToken[] = "_;5.]94-31==-%xT!^[$"; // trailing zero is included by sizeof()
     constexpr auto rsaExponent     = "65537";
     constexpr auto rsaKeyBitSize   = 1024;
--- login_server/main.cpp
+++ login_server/main.cpp
@@ -2,10 +2,12 @@
 /// @date      Created on 2026-02-13
 
 // Project includes
-#include "Packets.hpp"
+#include "Constants.hpp"
 #include "crypto/Blowfish.hpp"
 #include "crypto/Checksum.hpp"
 #include "crypto/Rsa.hpp"
+#include "network/Connection.hpp"
+#include "network/OpCodes.hpp"
 
 #include <l2cpp/Exception.hpp>
 #include <l2cpp/Typedefs.hpp>
@@ -13,150 +15,24 @@
 #include <l2cpp/network/SocketListener.hpp>
 
 // Third-party includes
-#include <boost/asio.hpp>
-#include <openssl/err.h>
+#include <boost/asio/signal_set.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include <openssl/rand.h>
-#include <openssl/rsa.h>
 #include <spdlog/spdlog.h>
 
-using boost::asio::ip::tcp;
 using l2cpp::Network::Packet;
-
-struct Connection
-{
-    u32             sessionId;
-    tcp::socket     socket;
-
-    std::unique_ptr<BIGNUM, decltype(&BN_free)>  bigNum;
-    std::unique_ptr<RSA,    decltype(&RSA_free)> rsaKey;
-
-    std::vector<byte> readBuffer;
-
-    std::string userName;
-    std::string password;
-
-    std::function<void()>                          onSocketClosed;
-    std::function<void(std::vector<byte> const &)> onPacketRead;
-
-    explicit Connection(tcp::socket socket)
-        : socket(std::move(socket))
-        , bigNum(nullptr, &BN_free)
-        , rsaKey(RSA_new(), &RSA_free)
-    {
-        static decltype(sessionId) nextSessionId = 0;
-        sessionId = nextSessionId++;
-
-        BIGNUM * n = nullptr;
-        BN_dec2bn(&n, "65537");
-        bigNum.reset(n);
-
-        RSA_generate_key_ex(rsaKey.get(), 1024, bigNum.get(), nullptr);
-        readBuffer.resize(sizeof(u16) + sizeof(u8) + RSA_size(rsaKey.get())); // size + opCode + rsa
-    }
-
-    bool operator==(Connection const & other) const { return sessionId == other.sessionId; }
-
-    void close()
-    {
-        if (socket.is_open())
-        {
-            boost::system::error_code ec;
-            if (socket.shutdown(socket.shutdown_both, ec))
-                SPDLOG_ERROR("shutdown error '{}' ({}): {}", ec.category().name(), ec.value(), ec.message());
-
-            if (socket.close(ec))
-                SPDLOG_ERROR("close error '{}' ({}): {}", ec.category().name(), ec.value(), ec.message());
-
-            if (onSocketClosed)
-                onSocketClosed();
-        }
-    }
-
-    void asyncReadNextPacket()
-    {
-        L2CPP_B_ASSERT(socket.is_open(), "Cannot read another packet: socket not open");
-        boost::asio::async_read(socket, boost::asio::buffer(readBuffer, sizeof(PacketHeader)),
-                                [this] (auto const & ec, auto) { onSizeRead(ec); });
-    }
-
-    void send(Packet & p, bool const encryptPacket = true)
-    {
-        static std::array<byte, 7> pad{};
-
-        // Align to 8 bytes then append checksum
-        if (auto const overflow = p.bodySize() % 8)
-            p << std::span(pad.data(), 8 - overflow);
-
-        p << Checksum::calculate({p.body().data(), p.bodySize()});
-        p.finalize();
-
-        if (encryptPacket)
-            Blowfish::encrypt(p.body());
-
-        boost::asio::write(socket, boost::asio::buffer(p.buffer()));
-        SPDLOG_INFO("'{}' ← 0x{:02x} ({:>3} bytes)", sessionId, p.opCode(), p.size());
-    }
-
-private:
-    void onSizeRead(boost::system::error_code const & ec)
-    {
-        if (!handleError(ec))
-            return close();
-
-        readBuffer.resize(*reinterpret_cast<PacketHeader const *>(readBuffer.data()));
-
-        auto const payload = std::span(readBuffer).subspan(sizeof(PacketHeader));
-        boost::asio::async_read(socket, boost::asio::buffer(payload), [this] (auto const & e, auto) { onBodyRead(e); });
-    }
-
-    void onBodyRead(boost::system::error_code const & ec)
-    {
-        if (!handleError(ec))
-            return close();
-
-        if (onPacketRead)
-            onPacketRead(readBuffer);
-    }
-
-    /// @returns @c true if the async operation can be executed again, else @c false
-    bool handleError(boost::system::error_code const & ec) const
-    {
-        switch (auto const code = ec.value())
-        {
-            case boost::system::errc::success:
-                return true;
-
-            case boost::asio::error::eof:
-                SPDLOG_INFO("Client {} disconnected by themselves", sessionId);
-                break;
-
-            case boost::asio::error::operation_aborted:
-                SPDLOG_INFO("Client {} disconnected by server", sessionId);
-                break;
-
-            default:
-                SPDLOG_ERROR("Something went wrong during packet body reading (code {}): {}", code, ec.message());
-                break;
-        }
-        return false;
-    }
-};
 
 static void sendInitPacket(Connection & conn)
 {
-    constexpr u32 protocol = 0xc621;
-
-    Packet p(SentPacket::Initialization);
-    p << conn.sessionId << protocol << Rsa::instance().modulus();
+    Packet p(ServerOpCode::Initialization);
+    p << conn.sessionId << Constants::protocol << Rsa::instance().modulus();
     conn.send(p, false);
-
-    conn.asyncReadNextPacket();
 }
 
 static void handleGameGuardPacket(Connection & conn)
 {
     constexpr u32 ignoreGg = 0x0b;
-    conn.send(Packet(SentPacket::GameGuard) << ignoreGg);
+    conn.send(Packet(ServerOpCode::GameGuard) << ignoreGg);
 }
 
 static void handleAuthPacket(Connection & conn)
@@ -170,7 +46,7 @@ static void handleAuthPacket(Connection & conn)
     catch (l2cpp::Exception const & e)
     {
         SPDLOG_ERROR("Rsa::decrypt failed:\n{}", l2cpp::formatExceptionStack(e));
-        return conn.send(Packet(SentPacket::AuthenticationFailed) << 0x01); // system error
+        return conn.send(Packet(ServerOpCode::AuthenticationFailed) << 0x01); // system error
     }
 
     conn.userName = reinterpret_cast<char const *>(body.data() + 0x62);
@@ -178,7 +54,7 @@ static void handleAuthPacket(Connection & conn)
 
     std::array<byte, sizeof(u64)> loginKey;
     RAND_bytes(loginKey.data(), static_cast<int>(loginKey.size()));
-    conn.send(Packet(SentPacket::AuthenticationSuccess) << loginKey);
+    conn.send(Packet(ServerOpCode::AuthenticationSuccess) << loginKey);
 }
 
 static void handleServerListPacket(Connection & conn)
@@ -199,7 +75,7 @@ static void handleServerListPacket(Connection & conn)
 
     constexpr u8 serverCount = 2;
 
-    Packet p(SentPacket::GameServerList);
+    Packet p(ServerOpCode::GameServerList);
     p << serverCount << 0_u8; // ?
 
     for (auto const srv : {&onlineServer, &offlineServer})
@@ -223,13 +99,9 @@ static void handleServerListPacket(Connection & conn)
 
 static void handleServerSelectionPacket(Connection & conn)
 {
-    u32 playOk1, playOk2;
-    RAND_bytes(reinterpret_cast<byte *>(&playOk1), sizeof(playOk1));
-    RAND_bytes(reinterpret_cast<byte *>(&playOk2), sizeof(playOk2));
-
-    Packet p(SentPacket::GameServerSelectionSuccess);
-    p << playOk1 << playOk2;
-    conn.send(p);
+    std::array<byte, sizeof(u64)> gameKey;
+    RAND_bytes(gameKey.data(), static_cast<int>(gameKey.size()));
+    conn.send(Packet(ServerOpCode::GameServerSelectionSuccess) << gameKey);
 }
 
 static void handlePacket(Connection & conn)
@@ -241,15 +113,15 @@ static void handlePacket(Connection & conn)
     auto const opCode = payload[0];
     SPDLOG_INFO("'{}' → 0x{:02x} ({:>3} bytes)", conn.sessionId, opCode, conn.readBuffer.size());
 
-    static std::unordered_map<RecvPacket, void(*)(Connection &)> const handlers
+    static std::unordered_map<ClientOpCode, void(*)(Connection &)> const handlers
     {
-        { RecvPacket::Authentication,      &handleAuthPacket            },
-        { RecvPacket::GameServerSelection, &handleServerSelectionPacket },
-        { RecvPacket::GameServerList,      &handleServerListPacket      },
-        { RecvPacket::GameGuard,           &handleGameGuardPacket       },
+        { ClientOpCode::Authentication,      &handleAuthPacket            },
+        { ClientOpCode::GameServerSelection, &handleServerSelectionPacket },
+        { ClientOpCode::GameServerList,      &handleServerListPacket      },
+        { ClientOpCode::GameGuard,           &handleGameGuardPacket       },
     };
 
-    if (auto const it = handlers.find(static_cast<RecvPacket>(opCode)); it != handlers.end()) try
+    if (auto const it = handlers.find(static_cast<ClientOpCode>(opCode)); it != handlers.end()) try
     {
         (*it->second)(conn);
     }
@@ -286,6 +158,9 @@ int main() try
     boost::asio::signal_set signals(ioContext, SIGINT);
     signals.async_wait([&] (auto const &, auto)
     {
+        SPDLOG_INFO("Shutdown sequence requested");
+
+        SPDLOG_INFO("Kicking players out");
         for (auto & conn : connections)
         {
             conn.onSocketClosed = nullptr;
@@ -295,18 +170,20 @@ int main() try
         listener.shutdown();
     });
 
-    auto onSocketAccepted = [&] (tcp::socket && socket)
+    auto onSocketAccepted = [&] (boost::asio::ip::tcp::socket && socket)
     {
         SPDLOG_INFO("Client connected!");
         auto & conn = connections.emplace_back(std::move(socket));
         conn.onSocketClosed = [&] { connections.remove(conn);          };
         conn.onPacketRead   = [&] (auto const &) { handlePacket(conn); };
+
         sendInitPacket(conn);
+        conn.asyncReadNextPacket();
     };
-    if (!listener.listen("127.0.0.1", 2106, std::move(onSocketAccepted)))
+    if (!listener.listen(Constants::host, Constants::port, std::move(onSocketAccepted)))
         return EXIT_FAILURE;
 
-    SPDLOG_INFO("Listening for clients on 127.0.0.1:2106");
+    SPDLOG_INFO("Listening for clients on {}:{}", Constants::host, Constants::port);
     ioContext.run();
 
     SPDLOG_INFO("Goodbye!");
