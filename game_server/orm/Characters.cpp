0942d7960e4d8ada73bf66bd406fc645ea4f968b
Chnossos <chnossos@gmail.com>
2026-05-14 10:37:44 +0200

    add: load inventory

--- game_server/orm/Characters.cpp
+++ game_server/orm/Characters.cpp
@@ -8,9 +8,17 @@
 #include "../game/components/CharacterStatus.hpp"
 #include "../game/components/Position.hpp"
 #include "../game/components/Stats.hpp"
+#include "../game/inventory/Item.hpp"
+#include "../game/inventory/ItemStorage.hpp"
+#include "../game/inventory/ItemTemplateDirectory.hpp"
 #include "../utils/Conversion.hpp"
 
 #include <l2cpp/services/Database.hpp>
+
+// Third-party includes
+#include <spdlog/spdlog.h>
+
+static void loadInventory(u32 characterId, Character & c);
 
 void Orm::saveCharacter(Character const & c)
 {
@@ -62,7 +70,8 @@ void Orm::loadCharacter(Character & c)
     // Here we do not load the preview fields again, we load the rest in order to enter the World
     SQLite::Statement query(Database::instance(), R"(
         SELECT
-            title
+            id
+          , title
           , orientation
         FROM
             characters
@@ -76,4 +85,43 @@ void Orm::loadCharacter(Character & c)
 
     c.setTitle(Utils::toWideString(query.getColumn("title").getString()));
     c.setOrientation(query.getColumn("orientation").getInt());
+
+    loadInventory(query.getColumn("id").getUInt(), c);
+}
+
+void loadInventory(u32 const characterId, Character & c)
+{
+    SQLite::Statement query{Database::instance(), R"(
+        SELECT
+            template_id
+          , quantity
+          , enchant_level
+        FROM
+            items
+        WHERE
+            owner_id = :character_id AND storage_id = 0 AND equipped = FALSE
+    )"};
+    query.bind(":character_id", characterId);
+
+    auto & inventory = c.inventory();
+    while (query.executeStep())
+    {
+        auto const templateId   = query.getColumn("template_id").getUInt();
+        auto const itemTemplate = ItemTemplateDirectory::find(templateId);
+        if (!itemTemplate)
+        {
+            SPDLOG_ERROR("ItemTemplate id '{}' not loaded but a character requests it", templateId);
+            continue;
+        }
+
+        auto const enchantLevel = query.getColumn("enchant_level");
+        auto const quantity     = query.getColumn("quantity");
+
+        Item item;
+        item.tmplate      = std::move(*itemTemplate);
+        item.enchantLevel = enchantLevel.isNull() ? 0 : static_cast<u8>(enchantLevel.getUInt());
+        item.quantity     = quantity    .isNull() ? 1 : quantity.getUInt();
+
+        inventory.add(std::move(item));
+    }
 }
